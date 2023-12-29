519949932f8910ee67977e0b10e983246154c3df
Torah <95339155+Termng@users.noreply.github.com>
2023-12-29 23:17:33 +0100

    set up mongo db

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
@@ -12,7 +13,20 @@ todos = db.todos
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        content = request.form['todoField']
+        degree = request.form['degree']
+        todos.insert_one({'todoField' : content, 'degree': degree })
+        return redirect(url_for('home'))
+    
+    all_todos = todos.find()
+    return render_template('home.html', todos=all_todos)
+
+@app.post ('/<id>/del/')
+def delete(id):
+    todos.delete_one({'_id': ObjectId(id)})
+    return redirect(url_for('home'))
+
 
 
 if __name__ == '__main__':
